1d908fb43ff95fea3444d2c81168b327f70672ee
kolbyboesel <62734896+kolbyboesel@users.noreply.github.com>
2023-05-02 11:31:57 -0500

    Updated site to run on it's own during final exams

--- Pages/SportsAI.cshtml.cs
+++ Pages/SportsAI.cshtml.cs
@@ -11,54 +11,5 @@ public class SportsAIModel : PageModel
 
     public async Task OnPostAsync()
     {
-        // Replace YOUR_API_KEY with your actual OpenAI API key
-        string apiKey = "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";
-        string endpoint = "https://api.openai.com/v1/completions";
-
-        var client = new RestClient(endpoint);
-        var request = new RestRequest("POST");
-        request.AddHeader("Authorization", $"Bearer {apiKey}");
-        request.AddHeader("Content-Type", "application/json");
-
-        var body = new
-        {
-            prompt = UserInput,
-            temperature = 0.7,
-            max_tokens = 60,
-            n = 1,
-            stop = "\n"
-        };
-
-        request.AddJsonBody(body);
-
-        RestResponse response = await client.PostAsync(request);
-
-        if (response.StatusCode == System.Net.HttpStatusCode.OK)
-        {
-            var openAIResponse = JsonSerializer.Deserialize<OpenAIResponse>(response.Content);
-            string botResponse = openAIResponse.choices[0].text;
-            ViewData["BotResponse"] = botResponse;
-
-        }
-        else
-        {
-            ViewData["BotResponse"] = response.StatusCode;
-        }
     }
-}
-
-
-public class OpenAIResponse
-{
-    public List<OpenAIChoice> choices { get; set; }
-    public string created { get; set; }
-    public string id { get; set; }
-}
-
-public class OpenAIChoice
-{
-    public float logprobs { get; set; }
-    public float[] logprobs_array { get; set; }
-    public float finish_reason { get; set; }
-    public string text { get; set; }
 }
--- wwwroot/js/kolbysBets.js
+++ wwwroot/js/kolbysBets.js
@@ -2,47 +2,12 @@ function showKolbysNBA() {
 
   let htmlSegment = `<div class="bestBetHeader"><div class="headerText">Kolby's NBA Bets -- <a href="https://docs.google.com/spreadsheets/d/1rz1qH7BAJCdIFsAlwKhCJBKv5SgXSlcmZmICXENyqbQ/edit#gid=698726683">Click To View Past Picks</a></div></div>
   <div class="kolbyPicksFullCont">`;
-  htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
-   <div class="pick">
-        <div class="pickNo">1. </div>
-        <div class="pickName">Parlay (Celtics Ml) / (76ers/Celtics O 214.5)</div>
-        <div class="pickLine">+131</div>
-      </div>
-    </div>
-  </div>`;
-
-  htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
-  <div class="pick">
-       <div class="pickNo">2. </div>
-       <div class="pickName">Jaylen Brown O 24.5 Points / James Harden O 8.5 Assists</div>
-       <div class="pickLine">+208</div>
-     </div>
-   </div>
- </div>`;
 
  htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
   <div class="pick">
-       <div class="pickNo">3. </div>
-       <div class="pickName">Malcom Brogdon O 3.5 Rebounds / P.J. Tucker O 4.5 Points</div>
-       <div class="pickLine">+274</div>
-     </div>
-   </div>
- </div>`;
-
- htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
-  <div class="pick">
-       <div class="pickNo">4. </div>
-       <div class="pickName">Nikola Jokic O 8.5 Assists / Chris Paul O 1.5 Steals</div>
-       <div class="pickLine">+242</div>
-     </div>
-   </div>
- </div>`;
-
- htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
-  <div class="pick">
-       <div class="pickNo">5. </div>
-       <div class="pickName">Jamal Murray O 24.5 Points / Kevin Durant U 28.5 Points</div>
-       <div class="pickLine">+264</div>
+       <div class="pickNo">1. </div>
+       <div class="pickName">Kolby's Picks Available in Google Sheet Above</div>
+       <div class="pickLine">TBA</div>
      </div>`;
 
   htmlSegment += `
@@ -58,20 +23,12 @@ function showKolbysMLB() {
 
   let htmlSegment = `<div class="bestBetHeader"><div class="headerText">Kolby's MLB Bets -- <a href="https://docs.google.com/spreadsheets/d/1rz1qH7BAJCdIFsAlwKhCJBKv5SgXSlcmZmICXENyqbQ/edit#gid=0">Click To View Past Picks</a></div></div>
   <div class="kolbyPicksFullCont">`;
-  htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
-   <div class="pick">
-        <div class="pickNo">1. </div>
-        <div class="pickName">Yankees Ml</div>
-        <div class="pickLine">-131</div>
-      </div>
-    </div>
-  </div>`;
 
   htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
    <div class="pick">
-        <div class="pickNo">2. </div>
-        <div class="pickName">Guardians / Yankees O 8.5</div>
-        <div class="pickLine">-107</div>
+        <div class="pickNo">1. </div>
+        <div class="pickName">Kolby's Picks Available in Google Sheet Above</div>
+        <div class="pickLine">TBA</div>
       </div>`;
 
   htmlSegment += `</div>
@@ -89,7 +46,7 @@ function showKolbysNHL() {
   htmlSegment += `<div class="outerKolbysPicks"><div class="kolbysPicks">
    <div class="pick">
         <div class="pickNo">1. </div>
-        <div class="pickName">No NHL Today</div>
+        <div class="pickName">Kolby's Picks Available in Google Sheet Above</div>
         <div class="pickLine">TBA</div>
       </div>`;
 
